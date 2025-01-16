4ce764e9112e8aeb5f35e08be014402639fab88c
João Marcus Reis <joaosenareis@gmail.com>
2025-01-16 15:00:11 -0300

    The base class to the Layers of the Neural Network

--- src/layer.py
+++ src/layer.py
@@ -1,10 +1,10 @@
 class Layer:
     """
-    Abstract class made to be a regular layer
+    Abstract class made to be a regular layer.
     """
     def __init__(self):
         """
-        Initializes an object of the Layer class
+        Initializes an object of the Layer class.
         """
         self.input = None
         self.output = None
