c0d9e01c52c28219b8bbe6eb13066ea6e309e017
João Marcus Reis <joaosenareis@gmail.com>
2025-01-19 18:38:09 -0300

    All the activation functions

--- src/activations.py
+++ src/activations.py
@@ -9,17 +9,31 @@ def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def sigmoid_prime(x):
-    """"""
+    """
+    Returns the derivative of the sigmoid function
+    """
     return np.exp(-x)/((1 + np.exp(-x)) ** 2)
 
 def tanh(x):
+    """
+    Returns the hiperbolycal tanget of x
+    """
     return np.tanh(x)
 
 def tanh_prime(x):
+    """
+    Returns the derivative of the hyperbolic tangent
+    """
     return 1 - tanh(x) ** 2
 
 def relu(x):
+    """
+    Returns the value of the RELU function.
+    """
     return np.maximum(x, 0)
 
 def relu_prime(x):
+    """
+    Returns the derivative of the RELU function
+    """
     return np.array(x >= 0).astype('int')
